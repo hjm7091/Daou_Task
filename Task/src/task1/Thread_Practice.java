8b910d1919fb6ed07f77b27de3412b59240ae307
hjm7091 <hjm7091@naver.com>
2019-12-08 18:51:51 +0900

    Thread_Practice commit

---
+++ Task/src/task1/Thread_Practice.java
@@ -0,0 +1,104 @@
+package task1;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+class MyThread extends Thread{
+	
+	private String path;
+	private boolean state = true;
+	private long pos = -1;
+	
+	public MyThread(String path) {
+		this.path = path;
+	}
+	
+	public void run() {
+		while(state) {
+			try {
+				RandomAccessFile RA = new RandomAccessFile(path, "r");
+				String new_path = get_path(path);
+				BufferedWriter out = new BufferedWriter(new FileWriter(new_path, true));
+				if(pos == -1) {
+					String line = "";
+					while((line = RA.readLine()) != null) {
+						System.out.println(line);
+					}
+					long now_pos = RA.length();
+					System.out.println("파일 크기 : " + now_pos);
+					byte[] data = new byte[(int)now_pos];
+					RA.seek(0);
+					RA.read(data);
+					out.write("처음 내용 : " + new String(data));
+					out.newLine();
+					out.flush();
+					pos = now_pos;
+				}
+				else {
+					String line = "";
+					while((line = RA.readLine()) != null) {
+						System.out.println(line);
+					}
+					long now_pos = RA.length();
+					System.out.println("파일 크기 : " + now_pos);
+					long diff = now_pos - pos;
+					if(diff > 0) {
+						System.out.println("크기가 커졌습니다.");
+						byte[] data = new byte[(int) diff]; 
+						RA.seek(now_pos - diff);
+						RA.read(data);
+						out.write("새로운 내용 : " + new String(data));
+						out.newLine();
+						out.flush();
+					}
+					else if(now_pos == pos)
+						System.out.println("크기가 그대로입니다.");
+					pos = now_pos;
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println("파일을 찾을 수 없습니다.");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				sleep(1000 * 5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+				System.out.println("종료");
+				state = false;
+			}
+		}
+	}
+	
+	public String get_path(String path) {
+		int last = path.lastIndexOf("\\");
+		return path.substring(0, last+1) + "result.txt";
+	}
+}
+
+public class Thread_Practice {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("경로 입력 : ");
+		String path = input.next();
+		MyThread t = new MyThread(path);
+		t.start();
+		boolean flag = true;
+		System.out.println("종료하려면 end를 입력하세요.");
+		while(flag) {
+			String s = input.next();
+			if(s.equals("end")) {
+				t.interrupt();
+				flag = false;
+			}
+		}
+	}
+
+}
